aa4ae477ee33010cf931b6da6bb5b9ae2efde74d
MockerPI <pavlenko.ivan@gmail.com>
2023-09-06 11:26:47 +0300

    even number

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World" + akor)
+num = int(input("Enter a number: "))
+
+if num > 0:
+    if num % 2 == 0:
+        result = "Positive odd number"
+        print(result)
+    else:
+        result = "Positive even number"
+        print(result)
+elif num < 0:
+    result = "Negative number"
+    print(result)
+else:
+    result = "It is zero"
+    print(result)
